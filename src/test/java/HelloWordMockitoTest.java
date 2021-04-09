516534735409766981c89113960462f092a29bd4
rodrigo.resque@gmail.com <rodrigo.resque@gmail.com>
2021-04-08 23:36:04 -0300

    feat: metodo teste leilao.

--- src/test/java/HelloWordMockitoTest.java
+++ src/test/java/HelloWordMockitoTest.java
@@ -1,2 +1,19 @@
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.model.Leilao;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
+
 public class HelloWordMockitoTest {
+
+    @Test
+    public void hello() {
+        LeilaoDao mock = Mockito.mock(LeilaoDao.class);
+        List<Leilao> leilaos = mock.buscarTodos();
+        Assert.assertTrue(leilaos.isEmpty());
+
+    }
 }
